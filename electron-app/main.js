0c238d4ab86b62ac86011b74b0dfe8bc8b40ea71
Austin Franco <austin@Austins-MacBook-Air.local>
2026-05-28 11:53:42 -0700

    created grid

--- electron-app/main.js
+++ electron-app/main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   const win = new BrowserWindow({
     width: 1024,
-    height: 768,
+    height: 850,
     resizable: false,
     maximizable: false,
     fullscreenable: false,
